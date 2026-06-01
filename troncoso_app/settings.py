d579cccd5f9912a350e70ef55d756cae88f97eea
jojimenez2025 <jljimenez2024@gmail.com>
2026-06-01 12:17:09 -0600

    Configura settings para PythonAnywhere

--- troncoso_app/settings.py
+++ troncoso_app/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-lp!f4-ukj$j6rc4m15h*iltk$ftb(nqwswvsg7-g6j9a2p^uf#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "jojimenez2026.pythonanywhere.com",
+]
 
 
 # Application definition
@@ -121,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
